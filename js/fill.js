4555b7097d97cb5c67fc3de519d3545afff4d3f6
evan-howie <trolleroller5@gmail.com>
2023-11-17 20:01:53 -0500

    split up flood fill to a web worker thread

--- js/fill.js
+++ js/fill.js
@@ -1,5 +1,4 @@
 import { hexToRGBA } from "./utils.js";
-import Queue from "./queue.js";
 
 export default class Fill {
   constructor(canvas, color, threshold) {
@@ -10,99 +9,44 @@ export default class Fill {
     // Bind event handlers to ensure they have the correct context
     this.mousedown = this.mousedown.bind(this);
 
+    // Initialize the worker
+    this.fillWorker = new Worker("./js/fillWorker.js", { type: "module" });
+
+    this.fillWorker.addEventListener(
+      "message",
+      function (e) {
+        const screen = e.data.screen;
+        // Apply the received image data back to the canvas
+        this.c.ctx.putImageData(
+          new ImageData(new Uint8ClampedArray(screen), this.c.w, this.c.h),
+          0,
+          0
+        );
+        this.c.pushState();
+      }.bind(this)
+    );
+
     // Event listeners for mouse events
     this.c.canvas.addEventListener("mousedown", this.mousedown);
-  }
-
-  isValid(screen, x, y, w, h, source) {
-    if (x < 0 || x >= w || y < 0 || y >= h) return false;
-    if (
-      Math.abs(screen[(y * this.c.w + x) * 4 + 0] - source[0]) / 255 >=
-      this.threshold
-    )
-      return false;
-    if (
-      Math.abs(screen[(y * this.c.w + x) * 4 + 1] - source[1]) / 255 >=
-      this.threshold
-    )
-      return false;
-    if (
-      Math.abs(screen[(y * this.c.w + x) * 4 + 2] - source[2]) / 255 >=
-      this.threshold
-    )
-      return false;
-    if (
-      Math.abs(screen[(y * this.c.w + x) * 4 + 3] - source[3]) / 255 >=
-      this.threshold
-    )
-      return false;
-    return true;
-  }
-
-  setPixel(screen, x, y, color) {
-    screen[(y * this.c.w + x) * 4 + 0] = color[0];
-    screen[(y * this.c.w + x) * 4 + 1] = color[1];
-    screen[(y * this.c.w + x) * 4 + 2] = color[2];
-    screen[(y * this.c.w + x) * 4 + 3] = color[3];
-  }
-
-  fill(screen, x, y, w, h, source, color) {
-    let queue = new Queue();
-    queue.enqueue([x, y]);
-    let filled = new Array(screen.length >> 2).fill(false);
-
-    console.log(window.performance.now());
-    while (!queue.isEmpty()) {
-      const [currX, currY] = queue.dequeue();
-      const pos = currY * this.c.w + currX;
-
-      // Skip if the pixel is already filled or not valid
-      if (filled[pos] || !this.isValid(screen, currX, currY, w, h, source)) {
-        continue;
-      }
-
-      this.setPixel(screen, currX, currY, color);
-      filled[pos] = true; // Mark the pixel as filled
-
-      // Check and queue the neighboring pixels
-      if (this.isValid(screen, currX + 1, currY, w, h, source)) {
-        queue.enqueue([currX + 1, currY]);
-      }
-
-      if (this.isValid(screen, currX - 1, currY, w, h, source)) {
-        queue.enqueue([currX - 1, currY]);
-      }
-
-      if (this.isValid(screen, currX, currY + 1, w, h, source)) {
-        queue.enqueue([currX, currY + 1]);
-      }
-
-      if (this.isValid(screen, currX, currY - 1, w, h, source)) {
-        queue.enqueue([currX, currY - 1]);
-      }
-    }
-    console.log(window.performance.now());
   }
 
   mousedown(e) {
     const screen = this.c.ctx.getImageData(0, 0, this.c.w, this.c.h).data;
     const pos = (e.offsetY * this.c.w + e.offsetX) * 4;
     const source = screen.slice(pos, pos + 4);
-    this.fill(
+    const color = hexToRGBA(this.color);
+
+    // Send the data to the worker
+    this.fillWorker.postMessage({
       screen,
-      e.offsetX,
-      e.offsetY,
-      this.c.w,
-      this.c.h,
+      x: e.offsetX,
+      y: e.offsetY,
+      w: this.c.w,
+      h: this.c.h,
       source,
-      hexToRGBA(this.color)
-    );
-    this.c.ctx.putImageData(
-      new ImageData(new Uint8ClampedArray(screen), this.c.w, this.c.h),
-      0,
-      0
-    );
-    this.c.pushState();
+      color,
+      threshold: this.threshold,
+    });
   }
 
   setFill(color) {
---
+++ js/fillWorker.js
@@ -0,0 +1,65 @@
+import Queue from "./queue.js";
+
+function isValid(screen, x, y, w, h, source, threshold) {
+  if (x < 0 || x >= w || y < 0 || y >= h) return false;
+  if (Math.abs(screen[(y * w + x) * 4 + 0] - source[0]) / 255 >= threshold)
+    return false;
+  if (Math.abs(screen[(y * w + x) * 4 + 1] - source[1]) / 255 >= threshold)
+    return false;
+  if (Math.abs(screen[(y * w + x) * 4 + 2] - source[2]) / 255 >= threshold)
+    return false;
+  if (Math.abs(screen[(y * w + x) * 4 + 3] - source[3]) / 255 >= threshold)
+    return false;
+  return true;
+}
+
+function setPixel(screen, x, y, w, color) {
+  screen[(y * w + x) * 4 + 0] = color[0];
+  screen[(y * w + x) * 4 + 1] = color[1];
+  screen[(y * w + x) * 4 + 2] = color[2];
+  screen[(y * w + x) * 4 + 3] = color[3];
+}
+
+self.addEventListener("message", function (e) {
+  const { screen, x, y, w, h, source, color, threshold } = e.data;
+
+  let queue = new Queue();
+  queue.enqueue([x, y]);
+  let filled = new Array(screen.length >> 2).fill(false);
+
+  while (!queue.isEmpty()) {
+    const [currX, currY] = queue.dequeue();
+    const pos = currY * w + currX;
+
+    // Skip if the pixel is already filled or not valid
+    if (
+      filled[pos] ||
+      !isValid(screen, currX, currY, w, h, source, threshold)
+    ) {
+      continue;
+    }
+
+    setPixel(screen, currX, currY, w, color);
+    filled[pos] = true; // Mark the pixel as filled
+
+    // Check and queue the neighboring pixels
+    if (isValid(screen, currX + 1, currY, w, h, source, threshold)) {
+      queue.enqueue([currX + 1, currY]);
+    }
+
+    if (isValid(screen, currX - 1, currY, w, h, source, threshold)) {
+      queue.enqueue([currX - 1, currY]);
+    }
+
+    if (isValid(screen, currX, currY + 1, w, h, source, threshold)) {
+      queue.enqueue([currX, currY + 1]);
+    }
+
+    if (isValid(screen, currX, currY - 1, w, h, source, threshold)) {
+      queue.enqueue([currX, currY - 1]);
+    }
+  }
+
+  // Post the modified screen data back to the main thread
+  self.postMessage({ screen });
+});
